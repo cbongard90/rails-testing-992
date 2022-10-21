4ba46c36e67c2c9b64cf92c9ca7880699034be3b
Christian Bongard <christian.bongard90@gmail.com>
2022-10-21 09:15:44 +0400

    add product test

--- test/system/products_test.rb
+++ test/system/products_test.rb
@@ -5,4 +5,19 @@ class ProductsTest < ApplicationSystemTestCase
     visit root_url # "/"
     assert_selector "h1", text: "Awesome Products"
   end
+
+  test "lets a signed in user create a new product" do
+    login_as users(:george)
+    visit "/products/new"
+    # save_screenshot
+
+    fill_in "product_name", with: "Le Wagon"
+    fill_in "product_tagline", with: "Change your life: Learn to code"
+    # save_screenshot
+
+    click_on "Create Product"
+    # save_screenshot
+
+    assert_text "Change your life: Learn to code"
+  end
 end
